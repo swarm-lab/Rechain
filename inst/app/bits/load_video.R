e42b5c54e58d0b575345a90130d6c3b6ae285913
Simon Garnier <garnier@njit.edu>
2019-07-15 11:00:26 +0200

    Fixes video loading issues

--- inst/app/bits/load_video.R
+++ inst/app/bits/load_video.R
@@ -1,18 +1,17 @@
 shinyFileChoose(input, "the_video", session = session,
                 roots = c(wd = normalizePath(ifelse(.Platform$OS.type == "unix", "~/", "~/.."))))
 
-observe({
-  if (input$the_video > 0) {
-    isolate({
-      path <- parseFilePaths(roots = c(wd = normalizePath(ifelse(.Platform$OS.type == "unix",
-                                                                 "~/", "~/.."))), input$the_video)
+observeEvent(input$the_video, {
+  path <- parseFilePaths(roots = c(wd = normalizePath(ifelse(.Platform$OS.type == "unix",
+                                                             "~/", "~/.."))), input$the_video)
 
-      if (file.exists(paste0(as.character(path$datapath), ".csv"))) {
-        the_dat <<- suppressMessages(read_csv(paste0(as.character(path$datapath), ".csv")))
-        updateSliderInput(session, "the_frame_slider", value = max(the_dat$frame, na.rm = TRUE))
-        react$the_video <- video(as.character(path$datapath))
-        react$update_plot <- react$update_plot + 1
-      }
-    })
+  if (nrow(path) > 0) {
+    if (file.exists(paste0(as.character(path$datapath), ".csv"))) {
+      the_dat <<- suppressMessages(read_csv(paste0(as.character(path$datapath), ".csv")))
+      updateSliderInput(session, "the_frame_slider", value = max(the_dat$frame, na.rm = TRUE))
+    }
+
+    react$the_video <- video(as.character(path$datapath))
+    react$update_plot <- react$update_plot + 1
   }
 })
